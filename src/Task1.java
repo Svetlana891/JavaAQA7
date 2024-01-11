7a44fce341c1e868f8ffc60473d6c3ee4b812176
Svetlana Muzychenko <svetlanamuzychenko333@gmail.com>
2024-01-11 11:51:30 +0200

    add new hw1

---
+++ src/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        a+=2;
+        b-=2;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
