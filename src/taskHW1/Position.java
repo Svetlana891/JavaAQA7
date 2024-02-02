b0d8b863d55d14caf124b4e25abf619388e7a183
Svetlana Muzychenko <svetlanamuzychenko333@gmail.com>
2024-02-02 20:14:42 +0200

    add new task

---
+++ src/taskHW1/Position.java
@@ -0,0 +1,16 @@
+package taskHW1;
+
+public class Position {
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 9;
+
+        a = a - b;
+        b = a + b;
+        a = (- a) + b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
