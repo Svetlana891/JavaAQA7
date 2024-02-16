a7f0289e7dae29f874ee91578915ab9317288624
Svetlana Muzychenko <svetlanamuzychenko333@gmail.com>
2024-02-17 00:25:26 +0200

    added exit condition

--- src/task4_1/Task1.java
+++ src/task4_1/Task1.java
@@ -1,25 +1,28 @@
 package task4_1;
 
+import java.util.Scanner;
 
-import java.util.Arrays;
-        import java.util.Scanner;
-        import java.util.Random;
 public class Task1 {
+    /*
+    Написать программу, которая будет считывать введенные пользователем слова с клавиатуры слова, и склеивать их в одно
+    предложение до тех пор, пока пользователь не введет с клавитуры слово STOP. Bce слова введенные до этого должны
+    отобразится в консоли одним предложением.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите ваше слово");
-        String word = scanner.next();
-        Random random = new Random();
-        int x = random.nextInt(10, 100);
+        String text = "";
 
-        String[] words = new String[x];
-        for (int i = 0; i < words.length; i++) {
-            words[i] = word;
-            System.out.print(words[i]);
-            word = scanner.next();
-            if (word.equals("STOP")) {
-                System.out.println(Arrays.toString(words));
+        while (true){
+            System.out.println("Введите ваше слово");
+            String word = scanner.next();
+
+            if(word.equals("STOP")){
+                break;
             }
+            text = text + word + " ";
+            System.out.println(text);
+
+
         }
 
 
