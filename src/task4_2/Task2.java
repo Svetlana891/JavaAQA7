f4e51b95f770e1b2baedfbbf8442889bcfbca8af
Svetlana Muzychenko <svetlanamuzychenko333@gmail.com>
2024-02-17 01:02:40 +0200

    add new change

--- src/task4_2/Task2.java
+++ src/task4_2/Task2.java
@@ -2,17 +2,26 @@ package task4_2;
 
 import java.util.Scanner;
 public class Task2 {
+/*
+    Написать программу в которой пользователь вводит с клавиатуры число, а программа определяет, является
+    она полиндоомом или нет. И выводит данную информацию на экран.
+ */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         String numberN = Integer.toString(number);
 
-        for( int i = 0; i < numberN.length(); i++){
-            if(numberN.charAt(i) == numberN.charAt(numberN.length() - 1 - i++)){
-                System.out.println("Два числа равны");
+        boolean isPolindrom = true;
+        for(int i = 0; i < numberN.length() / 2; i++){
+            if(numberN.charAt(i) != numberN.charAt(numberN.length() - 1 - i)){
+                System.out.println("Это не полиндром");
+                isPolindrom = false;
+                break;
             }
         }
-        System.out.println("Это полиндром");
+        if(isPolindrom == true) {
+            System.out.println("Это полиндром");
+        }
 
     }
 }
