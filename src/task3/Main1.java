1da254ddec3e9db9a4850fb68388daf0f4812ab9
Svetlana Muzychenko <svetlanamuzychenko333@gmail.com>
2024-02-23 16:02:08 +0200

    add new change

--- src/task3/Main1.java
+++ src/task3/Main1.java
@@ -1,11 +1,11 @@
 package task3;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
         Father father1 = new Father("Mark", 200, Food.APPLE, Product.BEER, 3, 2);
-
+        double summ;
         try {
-            father1.getPrice();
+            summ = father1.getPrice();
         }catch (NotEnoughMoneyException e){
             System.out.println(e.getMessage());
         }catch (WrongProductException e){
