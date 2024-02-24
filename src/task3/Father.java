1b785e78b1e66b97cc44fd4de953c0ee4db35b07
Svetlana Muzychenko <svetlanamuzychenko333@gmail.com>
2024-02-25 00:40:19 +0200

    add new change

--- src/task3/Father.java
+++ src/task3/Father.java
@@ -31,38 +31,44 @@ public class Father {
         double summ;
         double summ1 = 0;
         double summ2 = 0;
+        double summ3;
+        double summ4;
 
         if (type1Count > 0) {
             if (type1.equals(ProductType1.APPLE)) {
-                summ1 = money - ProductType1.APPLE.getCost() * type1Count;
+                 summ1 = money - ProductType1.APPLE.getCost() * type1Count;
             } else if (type1.equals(ProductType1.POTATO)) {
-                summ1 = money - ProductType1.POTATO.getCost() * type1Count;
+                 summ1 = money - ProductType1.POTATO.getCost() * type1Count;
             } else if (type1.equals(ProductType1.MILK)) {
-                summ1 = money - ProductType1.MILK.getCost() * type1Count;
+                 summ1 = money - ProductType1.MILK.getCost() * type1Count;
             } else if (type1.equals(ProductType1.BEER)) {
-                summ1 = money - ProductType1.BEER.getCost() * type1Count;
+                 summ1 = money - ProductType1.BEER.getCost() * type1Count;
             } else if (type1.equals(ProductType1.TOBACCO)) {
-                summ1 = money - ProductType1.TOBACCO.getCost() * type1Count;
+                 summ1 = money - ProductType1.TOBACCO.getCost() * type1Count;
             }
         }
+        summ3 = money - summ1;
 
         if (type2Count > 0) {
             if (type2.equals(ProductType2.APPLE)) {
-                summ2 = summ1 - ProductType2.APPLE.getCost() * type2Count;
+                summ2 = money - ProductType2.APPLE.getCost() * type2Count;
             } else if (type2.equals(ProductType2.POTATO)) {
-                summ2 = summ1 - ProductType2.POTATO.getCost() * type2Count;
+                summ2 = money - ProductType2.POTATO.getCost() * type2Count;
             } else if (type2.equals(ProductType2.MILK)) {
-                summ2 = summ1 - ProductType2.MILK.getCost() * type2Count;
+                summ2 = money - ProductType2.MILK.getCost() * type2Count;
             } else if (type2.equals(ProductType2.BEER)) {
-                summ2 = summ1 - ProductType2.BEER.getCost() * type2Count;
+                summ2 = money - ProductType2.BEER.getCost() * type2Count;
             } else if (type2.equals(ProductType2.TOBACCO)) {
-                summ2 = summ1 - ProductType2.TOBACCO.getCost() * type2Count;
+                summ2 = money - ProductType2.TOBACCO.getCost() * type2Count;
             }
         }
+        summ4 = money - summ2;
+        summ = summ3 + summ4;
 
-        System.out.println("Уважаемый ...");
+        System.out.println("Уважаемый " + nameFather + " стоимость ваших покупок " + summ + ". Вы приобрели " + type1 +
+                " в количестве " + type1Count + ", и " + type2 + " в количестве " + type2Count);
 
-        summ = summ2;
+
         if (summ > money) {
             throw new NotEnoughMoneyException();
         } else if (type1.equals(ProductType1.BEER) || type2.equals(ProductType2.BEER)) {
--- src/task3/Main1.java
+++ src/task3/Main1.java
@@ -4,7 +4,6 @@ public class Main1 {
     public static void main(String[] args) {
         Father father1 = new Father("Mark", 200);
 
-
         double summ;
         try {
             summ = father1.buyProducts(3,4, ProductType1.APPLE, ProductType2.POTATO);
