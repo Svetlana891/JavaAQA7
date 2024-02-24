96e40a4ae8ab3df1c25887dc2aa3325d7cf2409f
Svetlana Muzychenko <svetlanamuzychenko333@gmail.com>
2024-02-24 23:35:51 +0200

    add new change

--- src/task3/Shop.java
+++ src/task3/Shop.java
@@ -5,7 +5,7 @@ public class Shop {
     private int type2Count;
 
 
-    public double buyProducts(Father father, int type1Count, int type2Count, ProductType1 Type1, ProductType2 Type2)
+    public double buyProducts(Father father, int type1Count, int type2Count, ProductType1 type1, ProductType2 type2)
             throws NotEnoughMoneyException, WrongProductException, IncorrectCountOfGoodsException {
 
         double summ;
@@ -13,29 +13,29 @@ public class Shop {
         double summ2 = 0;
 
         if (type1Count > 0) {
-            if (Type1.equals(ProductType1.APPLE)) {
+            if (type1.equals(ProductType1.APPLE)) {
                 summ1 = father.getMoney() - ProductType1.APPLE.getCost() * type1Count;
-            } else if (Type1.equals(ProductType1.POTATO)) {
+            } else if (type1.equals(ProductType1.POTATO)) {
                 summ1 = father.getMoney() - ProductType1.POTATO.getCost() * type1Count;
-            } else if (Type1.equals(ProductType1.MILK)) {
+            } else if (type1.equals(ProductType1.MILK)) {
                 summ1 = father.getMoney() - ProductType1.MILK.getCost() * type1Count;
-            } else if (Type1.equals(ProductType1.BEER)) {
+            } else if (type1.equals(ProductType1.BEER)) {
                 summ1 = father.getMoney() - ProductType1.BEER.getCost() * type1Count;
-            } else if (Type1.equals(ProductType1.TOBACCO)) {
+            } else if (type1.equals(ProductType1.TOBACCO)) {
                 summ1 = father.getMoney() - ProductType1.TOBACCO.getCost() * type1Count;
             }
         }
 
         if (type2Count > 0) {
-            if (Type2.equals(ProductType2.APPLE)) {
+            if (type2.equals(ProductType2.APPLE)) {
                 summ2 = father.getMoney() - ProductType2.APPLE.getCost() * type2Count;
-            } else if (Type2.equals(ProductType2.POTATO)) {
+            } else if (type2.equals(ProductType2.POTATO)) {
                 summ2 = father.getMoney() - ProductType2.POTATO.getCost() * type2Count;
-            } else if (Type2.equals(ProductType2.MILK)) {
+            } else if (type2.equals(ProductType2.MILK)) {
                 summ2 = father.getMoney() - ProductType2.MILK.getCost() * type2Count;
-            } else if (Type2.equals(ProductType2.BEER)) {
+            } else if (type2.equals(ProductType2.BEER)) {
                 summ2 = father.getMoney() - ProductType2.BEER.getCost() * type2Count;
-            } else if (Type2.equals(ProductType2.TOBACCO)) {
+            } else if (type2.equals(ProductType2.TOBACCO)) {
                 summ2 = father.getMoney() - ProductType2.TOBACCO.getCost() * type2Count;
             }
         }
@@ -45,7 +45,7 @@ public class Shop {
         summ = summ1 + summ2;
         if (summ > father.getMoney()) {
             throw new NotEnoughMoneyException();
-        } else if (Type1.equals(ProductType1.BEER) || Type2.equals(ProductType2.BEER)) {
+        } else if (type1.equals(ProductType1.BEER) || type2.equals(ProductType2.BEER)) {
             throw new WrongProductException();
         } else if (type1Count % 3 != 0 || type2Count % 3 != 0) {
             throw new IncorrectCountOfGoodsException();
